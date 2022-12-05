dd68d138ae95e51e2e15662b3293b28272470376
Wei <wayne606.huang@gmail.com>
2022-12-05 13:15:03 +0800

    12/5

--- Lab1/mysolver.py
+++ Lab1/mysolver.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 """
 
 This program solves Initial Value Problems (IVP).
@@ -17,7 +18,7 @@ Example Usage:
     t_eval =np.linspace(0,1,100)
 
     sol = solve_ivp(yderive_func, t_span, y0, 
-                    method="RK4",t_eval=t_eval, args=(K,M))
+                    method="RK4",t_eval=t_eval, args=(a,b,c))
 
 
     See `solve_ivp` for detailed description. 
@@ -41,19 +42,26 @@ def solve_ivp(derive_func, t_span, y0, method, t_eval, args):
 
     :return: array_like. solutions. 
 
-    Note: the structe of this function is to mimic the scipy.integrate
+    Note: the structure of this function is to mimic the scipy.integrate
           In the numerical scheme we designed, we didn't check the consistentcy between
           t_span and t_eval. Be careful. 
 
     """
 
     sol  = np.zeros((len(y0),len(t_eval))) # define the shape of the solution
-
-    #
-    # TODO:
-    #
-
+    time = t_span[0]
+    y    = y0 #why
+    
+    for n,t in enumerate(t_eval):
+        dt = t - time 
+        if (dt>=0):
+            #update
+            y = _update(derive_func,y,dt,t,method,*args)#?
+        #record
+        sol[:,n] = y #why
+        time += dt
     return sol
+    
 
 def _update(derive_func,y0, dt, t, method, *args):
     """
@@ -87,13 +95,10 @@ def _update_euler(derive_func,y0,dt,t,*args):
 
     :return: the next step solution y
 
-    """
-
-    #
-    # TODO:
-    #
-
-    return y0 # <- change here. just a placeholder
+    """   
+    k1 =  derive_func(t,y0,*args)
+    ynext = y0 + k1*dt
+    return ynext
 
 def _update_rk2(derive_func,y0,dt,t,*args):
     """
@@ -101,12 +106,11 @@ def _update_rk2(derive_func,y0,dt,t,*args):
 
     :return: the next step solution y
     """
-
-    #
-    # TODO:
-    #
-
-    return y0 # <- change here. just a placeholder
+    k1 = derive_func(t,y0,*args)
+    y1 = y0 + dt*k1
+    k2 = derive_func(t+dt,y1,*args)
+    ynext = y0 +0.5*dt*(k1+k2)
+    return ynext 
 
 def _update_rk4(derive_func,y0,dt,t,*args):
     """
@@ -114,12 +118,15 @@ def _update_rk4(derive_func,y0,dt,t,*args):
 
     :return: the next step solution y
     """
-
-    #
-    # TODO:
-    #
-
-    return y0 # <- change here. just a placeholder
+    dt2 = 0.5*dt
+    k1 = derive_func(t,y0,*args)
+    y1 = y0 + k1*dt2
+    k2 = derive_func(t+dt2,y1,*args)
+    y2 = y0 + k2*dt2
+    k3 = derive_func(t+dt2,y2,*args)
+    y3 = y0 + k3*dt
+    k4 = derive_func(t+dt,y3,*args)
+    return y0 + (dt/6)*(k1+2*k2+2*k3+k4)
 
 if __name__=='__main__':
 
@@ -150,22 +157,23 @@ if __name__=='__main__':
         :param M: the mass of the oscillator
 
         """
-
-        #
-        # TODO:
-        #
- 
-        return y # <- change here. just a placeholder
+        
+        yder = np.zeros(len(y))
+        yder[0] = y[1]
+        yder[1] = (-K/M)*y[0]
+        return yder      
 
     t_span = (0, 10)
     y0     = np.array([1,0])
-    t_eval = np.linspace(0,1,100)
-
-    K = 1
-    M = 1
+    t_eval = np.linspace(0,20,1000)
+    K      = 1
+    M      = 1
 
     sol = solve_ivp(oscillator, t_span, y0, 
-                    method="Euler",t_eval=t_eval, args=(K,M))
+                    method="RK4",t_eval=t_eval, args=(K,M))
 
     print("sol=",sol[0])
+    print(t_eval.size)
     print("Done!")
+    plt.plot(sol[0])
+    plt.show()
---
+++ Lab2/nbody.py
@@ -0,0 +1,518 @@
+from time import time
+import numpy as np
+import matplotlib.pyplot as plt
+from pathlib import Path
+import numba as na
+from numba import jit, njit, prange, set_num_threads
+
+"""
+
+This program solve 3D direct N-particles simulations 
+under gravitational forces. 
+
+This file contains two classes:
+
+1) Particles: describes the particle properties
+2) NbodySimulation: describes the simulation
+
+Usage:
+
+    Step 1: import necessary classes
+
+    from nbody import Particles, NbodySimulation
+
+    Step 2: Write your own initialization function
+
+    
+        def initialize(particles:Particles):
+            ....
+            ....
+            particles.set_masses(mass)
+            particles.set_positions(pos)
+            particles.set_velocities(vel)
+            particles.set_accelerations(acc)
+
+            return particles
+
+    Step 3: Initialize your particles.
+
+        particles = Particles(N=100)
+        initialize(particles)
+
+
+    Step 4: Initial, setup and start the simulation
+
+        simulation = NbodySimulation(particles)
+        simulation.setip(...)
+        simulation.evolve(dt=0.001, tmax=10)
+
+
+Author: Kuo-Chuan Pan, NTHU 2022.10.30
+For the course, computational physics lab
+
+"""
+
+class Particles:
+    """
+    
+    The Particles class handle all particle properties
+
+    for the N-body simulation. 
+
+    """
+    def __init__(self,N:int=100):
+        """
+        Prepare memories for N particles
+
+        :param N: number of particles.
+
+        By default: particle properties include:
+                nparticles: int. number of particles
+                _masses: (N,1) mass of each particle
+                _positions:  (N,3) x,y,z positions of each particle
+                _velocities:  (N,3) vx, vy, vz velocities of each particle
+                _accelerations:  (N,3) ax, ay, az accelerations of each partciel
+                _tags:  (N)   tag of each particle
+                _time: float. the simulation time 
+
+        """
+        #build array shape
+        self.nparticles = N 
+        self._time = 0
+        self._masses = np.ones((N,1))
+        self._positions = np.zeros((N,3))
+        self._velocities = np.zeros((N,3))
+        self._accelerations = np.zeros((N,3))
+        self._tags = np.linspace(1,N,N)
+        # self._Ek = np.zeros((N,1))
+        # self._U = np.zeros((N,1))
+        return
+
+    #getter
+
+    def get_time(self):
+        return self._time
+    def get_masses(self):
+        return self._masses
+    def get_positions(self):
+        return self._positions
+    def get_velocities(self):
+        return self._velocities 
+    def get_accelerations(self):
+        return self._accelerations
+    def get_tags(self): 
+        return self._tags
+    # def get_Ek(self): 
+    #     return self._Ek
+    # def get_U(self): 
+    #     return self._U
+
+    
+    #setter
+    def set_time(self,time):
+        self._time = time
+        return
+    def set_masses(self,masses):
+        self._masses = masses
+        return
+    def set_positions(self,positions):
+        self._positions = positions
+        return
+    def set_velocities(self,velocities):
+        self._velocities = velocities
+        return
+    def set_accelerations(self,accelerations):
+        self._accelerations = accelerations
+        return
+    def set_tags(self,tags): 
+        self._tags = tags
+        return
+    # def set_Ek(self,Ek): 
+    #     self._Ek = Ek
+    #     return
+    # def set_U(self,U): 
+    #     self._U = U
+
+
+    def output(self,fn, time):
+        """
+        Write simulation data into a file named "fn"
+
+
+        """
+        mass = self._masses
+        pos  = self._positions
+        vel  = self._velocities
+        acc  = self._accelerations
+        tag  = self._tags
+        # Ek   = self._Ek
+        # U    = self._U
+        header = """
+                ----------------------------------------------------
+                Data from a 3D direct N-body simulation. 
+
+                rows are i-particle; 
+                coumns are :mass, tag, x ,y, z, vx, vy, vz, ax, ay, az
+
+                NTHU, Computational Physics Lab
+
+                ----------------------------------------------------
+                """
+        header += "Time = {}".format(time)
+        np.savetxt(fn,(tag[:],mass[:,0],pos[:,0],pos[:,1],pos[:,2],
+                            vel[:,0],vel[:,1],vel[:,2],
+                            acc[:,0],acc[:,1],acc[:,2]),header=header)
+
+        return
+    
+    # def _Energy(self):
+    #     mass = particles.get_masses()
+    #     pos = particles.get_positions() # y0[0]
+    #     vel = particles.get_velocities()
+    #     G = 6.67e-8
+    #     Ek = 0.5*mass*vel**2
+    #     U  = mass*G*pos
+    #     posx = pos[:,0]
+    #     posy = pos[:,1]
+    #     posz = pos[:,2]
+    #     for i in range(N):
+    #         for j in range(N):
+    #             if(j>i):
+    #                 x = (posx[i] - posx[j])
+    #                 y = (posy[i] - posy[j])
+    #                 z = (posz[i] - posz[j])
+    #                 rsq = x**2 + y**2 + z**2
+    #                 U = -G*mass[i,0]*mass[j,0]/(rsq+0.01)
+    #     return np.sum(U), np.sum(Ek)
+
+
+
+
+class NbodySimulation:
+    """
+    
+    The N-body Simulation class.
+    
+    """
+
+    def __init__(self,particles:Particles):
+        """
+        Initialize the N-body simulation with given Particles.
+
+        :param particles: A Particles class.  
+        
+        """
+
+        # store the particle information
+        self.nparticles = particles.nparticles
+        self.particles  = particles
+
+        # Store physical information
+        self.time  = 0.0  # simulation time
+
+        # Set the default numerical schemes and parameters
+        self.setup()
+        
+        return
+
+    def setup(self, G=6.67e-8, #unit select
+                    rsoft=0.01, 
+                    method="RK4", 
+                    io_freq=10, 
+                    io_title="particles",
+                    io_screen=True,
+                    visualized=False):
+        """
+        Customize the simulation enviroments.
+
+        :param G: the graivtational constant
+        :param rsoft: float, a soften length
+        :param meothd: string, the numerical scheme
+                       support "Euler", "RK2", and "RK4"
+
+        :param io_freq: int, the frequency to outupt data.
+                        io_freq <=0 for no output. 
+        :param io_title: the output header
+        :param io_screen: print message on screen or not.
+        :param visualized: on the fly visualization or not. 
+        
+        """
+        #先設定class 
+        self.G = G
+        self.rsoft = rsoft
+        self.method = method
+        self.io_freq = io_freq
+        self.io_title = io_title
+        self.io_screen = io_screen
+        self.visualized = visualized
+        return 
+
+    def evolve(self, dt:float=0.01, tmax:float=1):
+        """
+
+        Start to evolve the system
+
+        :param dt: time step
+        :param tmax: the finial time
+        
+        """
+        # TODO:
+        self.dt = dt
+        self.tmax = tmax
+
+
+
+        method = self.method
+        if method=="Euler":
+            _update_particles = self._update_particles_euler
+        elif method=="RK2":
+            _update_particles = self._update_particles_rk2
+        elif method=="RK4":
+            _update_particles = self._update_particles_rk4
+        elif method=="LF":
+            _update_particles = self._update_particles_LF
+        else:
+            print("No such update meothd", method)
+            quit() 
+
+        # prepare an output folder for lateron output
+        io_folder = "data_"+self.io_title
+        Path(io_folder).mkdir(parents=True, exist_ok=True)
+        
+        # ====================================================
+        #
+        # The main loop of the simulation
+        #
+        # =====================================================
+
+        # TODO:
+        time = self.time
+        nsteps = np.ceil((tmax-time)/dt)
+        #np.ceil np.floor
+        for n in range(int(nsteps)):
+
+            if (time+dt) > tmax: dt = tmax - time
+            
+            #update_particles 
+            particles = self.particles
+            _update_particles(dt, particles)
+
+            #check io/visual
+            if (n % self.io_freq ==0):
+                if self.io_screen:
+                    print("n =",n, " time =",time, " dt =", dt)
+                
+                #visual
+                if self.visualized:
+                    pass
+
+                #output data
+                fn = io_folder+"/data_"+self.io_title+"_"+str(n).zfill(5)+".txt"
+                self.particles.output(fn,time)
+                
+                
+        
+
+
+            #update time
+            time += dt
+
+        print("Done!")
+        return
+
+    # def _calculate_acceleration(self, mass, pos):
+    #     """
+    #     Calculate the acceleration.
+    #     """
+    #     # TODO:
+    #     # acc =(1e13/86400**2)*np.ones((self.particles,3))
+    #     posx = pos[:,0]
+    #     posy = pos[:,1]
+    #     posz = pos[:,2]
+    #     G    = self.G
+    #     npts = self.nparticles
+    #     rsoft = self.rsoft
+    #     acc = np.zeros((npts,3)) #reset acc
+
+    #     for i in range(npts):
+    #         for j in range(npts):
+    #             if(j>i):
+    #                 x = (posx[i] - posx[j])
+    #                 y = (posy[i] - posy[j])
+    #                 z = (posz[i] - posz[j])
+    #                 rsq = x**2 + y**2 + z**2
+    #                 req = np.sqrt(x**2+y**2)
+    #                 force = -G*mass[i,0]*mass[j,0]/(rsq+rsoft)
+    #                 theta = np.arctan2(y,x)
+    #                 phi = np.arctan2(z,req)
+    #                 fx = force*np.cos(theta)*np.cos(phi)
+    #                 fy = force*np.sin(theta)*np.cos(phi)
+    #                 fz = force*np.sin(phi)
+    #                 acc[i,0] += fx/mass[i,0]
+    #                 acc[i,1] += fy/mass[i,0]
+    #                 acc[i,2] += fz/mass[i,0]
+
+    #                 acc[j,0] -= fx/mass[j,0]
+    #                 acc[j,1] -= fy/mass[j,0]
+    #                 acc[j,2] -= fz/mass[j,0]
+    #     return acc
+    def _update_particles_euler(self, dt, particles:Particles):
+        mass = particles.get_masses()
+        pos = particles.get_positions() # y0[0]
+        vel = particles.get_velocities()
+        acc = particles.get_accelerations()
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        G    = self.G
+        npts = self.nparticles
+        rsoft = self.rsoft
+        # acc = np.zeros((npts,3)) #reset acc
+        # acc = self._calculate_acceleration(mass,pos)
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        pos = pos + vel * dt # y1[0]
+        vel = vel + acc * dt 
+        # acc = self._calculate_acceleration(mass,pos)
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        particles.set_positions(pos)
+        particles.set_velocities(vel)
+        particles.set_accelerations(acc)
+        return particles
+
+    def _update_particles_rk2(self, dt, particles:Particles):
+        mass = particles.get_masses()
+        pos = particles.get_positions() # y0[0]
+        vel = particles.get_velocities()
+        acc = particles.get_accelerations()
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        G    = self.G
+        npts = self.nparticles
+        rsoft = self.rsoft
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        pos_1 = pos + vel*dt
+        vel_1 = vel + acc*dt
+        posx = pos_1[:,0]
+        posy = pos_1[:,1]
+        posz = pos_1[:,2]
+        acc_1 = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+
+        pos = pos+0.5*dt*(vel+vel_1)
+        vel = vel+0.5*dt*(acc+acc_1)
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        
+        particles.set_positions(pos)
+        particles.set_velocities(vel)
+        particles.set_accelerations(acc)
+        return particles
+
+    def _update_particles_rk4(self, dt, particles:Particles):
+        mass = particles.get_masses()
+        pos = particles.get_positions() # y0[0]
+        vel = particles.get_velocities()
+        acc = particles.get_accelerations()
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        G    = self.G
+        npts = self.nparticles
+        rsoft = self.rsoft
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        pos_1 = pos + 0.5*vel*dt
+        vel_1 = vel + 0.5*acc*dt
+        posx = pos_1[:,0]
+        posy = pos_1[:,1]
+        posz = pos_1[:,2]
+        acc_1 = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+
+        pos_2 = pos_1 + 0.5*vel_1*dt
+        vel_2 = vel_1 + 0.5*acc_1*dt
+        posx = pos_2[:,0]
+        posy = pos_2[:,1]
+        posz = pos_2[:,2]
+        acc_2 = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+
+        pos_3 = pos_2 + vel_2*dt
+        vel_3 = vel_2 + acc_2*dt
+        posx = pos_3[:,0]
+        posy = pos_3[:,1]
+        posz = pos_3[:,2]
+        acc_3 = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+
+        pos = pos+(1/6)*dt*(vel+2*vel_1+2*vel_2+vel_3)
+        vel = vel+(1/6)*dt*(acc+2*acc_1+2*acc_2+acc_3)
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        
+        particles.set_positions(pos)
+        particles.set_velocities(vel)
+        particles.set_accelerations(acc)
+        return particles
+    
+    def _update_particles_LF(self, dt, particles:Particles):
+        mass = particles.get_masses()
+        pos = particles.get_positions() # y0[0]
+        vel = particles.get_velocities()
+        acc = particles.get_accelerations()
+        posx = pos[:,0]
+        posy = pos[:,1]
+        posz = pos[:,2]
+        G    = self.G
+        npts = self.nparticles
+        rsoft = self.rsoft
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        pos = pos + vel * dt # y1[0] 
+        vel = vel + 0.5 * acc * dt
+        acc = _calculate_acceleration_kernel(mass, posx, posy, posz, acc, G, rsoft, npts)
+        particles.set_positions(pos)
+        particles.set_velocities(vel)
+        particles.set_accelerations(acc)
+        return particles
+    
+
+
+
+@jit(nopython=True)
+def _calculate_acceleration_kernel(mass, posx ,posy ,posz , acc, G, rsoft,npts):
+
+    for i in range(npts):
+            for j in range(npts):
+                if(j>i):
+                    x = (posx[i] - posx[j])
+                    y = (posy[i] - posy[j])
+                    z = (posz[i] - posz[j])
+                    rsq = x**2 + y**2 + z**2
+                    req = np.sqrt(x**2+y**2)
+                    force = -G*mass[i,0]*mass[j,0]/(rsq+rsoft**2)
+                    theta = np.arctan2(y,x)
+                    phi = np.arctan2(z,req)
+                    fx = force*np.cos(theta)*np.cos(phi)
+                    fy = force*np.sin(theta)*np.cos(phi)
+                    fz = force*np.sin(phi)
+
+                    acc[i,0] += fx/mass[i,0]
+                    acc[i,1] += fy/mass[i,0]
+                    acc[i,2] += fz/mass[i,0]
+
+                    acc[j,0] -= fx/mass[j,0]
+                    acc[j,1] -= fy/mass[j,0]
+                    acc[j,2] -= fz/mass[j,0]
+    return acc
+
+if __name__=='__main__':
+
+
+    # test Particles() here
+    particles = Particles(N=100)
+    # test NbodySimulation(particles) here
+    sim = NbodySimulation(particles=particles)
+    sim.setup(G=6.67e-8, io_freq=1)
+    sim.evolve(dt = 0.01, tmax=10)
+    print(sim.G)
+    print("Done")
